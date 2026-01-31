7d815e4e85699e48a014d71c1648487b83a224d4
Alejandro <a2provedo@gmail.com>
2026-01-31 12:00:29 -0500

    Initial Commit

---
+++ JobTrackerTests/JobTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  JobTrackerTests.swift
+//  JobTrackerTests
+//
+//  Created by Alejandro on 1/31/26.
+//
+
+import Testing
+@testable import JobTracker
+
+struct JobTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
