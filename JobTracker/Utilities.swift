6cce869d6ab176f3f68c94d0584011ae33a8584a
Alejandro <a2provedo@gmail.com>
2026-02-03 21:12:49 -0500

    Start work on Add New Job View

---
+++ JobTracker/Utilities.swift
@@ -0,0 +1,18 @@
+//
+//  Utilities.swift
+//  JobTracker
+//
+//  Created by Alejandro on 2/3/26.
+//
+
+import SwiftUI
+
+struct Utilities: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Utilities()
+}
